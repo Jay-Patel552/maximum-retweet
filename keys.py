8ccad279e49d4625d6a2ae3c9b3ecbc4022c35c3
Jay-Patel552 <jpatel996.jp@gmail.com>
2021-02-17 22:05:07 -0500

    keys

---
+++ keys.py
@@ -0,0 +1,4 @@
+consumer_API_key="T6JiJJ8qlDGh1qi9g2PHuEMZL"
+consumer_API_secret_key="hFCBzwYy2HfN4saqbqJpoQUmAEgyS2msBxF9DSihS5fIOnj3UH"
+Access_token="1359164352113344513-FNkHIGNjyPBZTipj70gyS5xQWOiWxg"
+Access_token_secret="QZJzvhqoOejazz0MnGtToGujFJHEZBS6Ti4zxxxOdVNWp" 
