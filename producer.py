cae1de94a3cd7fca13689eaf05cfe948ca048f31
Jay-Patel552 <jpatel996.jp@gmail.com>
2021-02-20 01:58:00 -0500

    .

--- producer.py
+++ producer.py
@@ -1,18 +1,49 @@
-import tweepy
-import keys as k
+from kafka import KafkaProducer
+from stream_obtain import twit_stream
+import sys
+import json
+import os
+import time
 
-auth=tweepy.OAuthHandler(k.consumer_API_key,k.consumer_API_secret_key)
-auth.set_access_token(k.Access_token,k.Access_token_secret)
+#producer=KafkaProducer(bootstrap_servers='localhost:9092')
 
-api=tweepy.API(auth)
-i=0
-public_tweets=api.get_streams(tweet_mode='extended')
-some_user=api.get_user('@elonmusk')
-print(some_user.screen_name)
-print(some_user.followers_count)
-print("")
-for tweet in public_tweets:
-    print(tweet.full_text)
-    i+=1
-    if i==10:
-        break
+if __name__=='__main__':
+    id_covid=0
+    id_covid_vaccine=0
+    batch_size=int(sys.argv[1])//10+1
+    req_rules=[{"value":"covid","tag":"covid123"},{"value":'"covid vaccine"',"tag":"vaccine"}]
+    req_rules2=[{"value":'"covid vaccine"',"tag":"vaccine"}]
+    req_rules1=[{"value":"covid","tag":"covid123"}]
+    ls_covid=[]
+    ls_vaccine=[]
+    while(1):
+        temp_ls=[]
+        if id_covid <= int(sys.argv[1]) and id_covid_vaccine<= int(sys.argv[1]):
+            json_response_list=twit_stream(req_rules,temp_ls,batch_size)
+            for json_response in json_response_list:
+                matching_rule=json_response.get("matching_rules")[0].get('tag')
+                if matching_rule=='covid123':
+                    id_covid+=1
+                    json_response.update({'ID':id_covid})
+                    ls_covid.append(json.dumps(json_response,sort_keys=True))
+                if matching_rule=='vaccine':
+                    id_covid_vaccine+=1
+                    json_response.update({'ID':id_covid_vaccine})
+                    ls_vaccine.append(json.dumps(json_response,sort_keys=True))
+        elif id_covid <= int(sys.argv[1]) and id_covid_vaccine > int(sys.argv[1]):
+            json_response_list=twit_stream(req_rules1,temp_ls,batch_size)
+            for json_response in json_response_list:
+                id_covid+=1
+                json_response.update({'ID':id_covid})
+                ls_covid.append(json.dumps(json_response,sort_keys=True))
+        elif id_covid > int(sys.argv[1]) and id_covid_vaccine <= int(sys.argv[1]):
+            json_response_list=twit_stream(req_rules2,temp_ls,batch_size)
+            for json_response in json_response_list:
+                id_covid_vaccine+=1
+                json_response.update({"ID":id_covid_vaccine})
+                ls_vaccine.append(json.dumps(json_response, sort_keys=True))
+        else:
+            break
+        time.sleep(0.34)
+    print(ls_covid)
+    print(ls_vaccine)
--- stream_obtain.py
+++ stream_obtain.py
@@ -3,30 +3,61 @@ import sys
 import requests
 import os
 
-def refine_rules(header):
+def refine_rules(header,req_rules):
     current_rules=requests.get("https://api.twitter.com/2/tweets/search/stream/rules",headers=header)
-    print(json.dumps(current_rules.json()))
-    rules=[{'value':'covid','tag':'covid'},{'value':'covid vaccine','tag':'covid vaccine'}]
-    to_add={'add':rules}
+    #print(json.dumps(current_rules.json()))
+    current_rules_json=current_rules.json()
+
+    if current_rules_json is None or "data" not in current_rules_json:
+        pass
+    else:
+        list_ids=list(map(lambda rule: rule["id"], current_rules_json["data"]))
+        #print(list_ids)
+        to_delete={"delete":{'ids':list_ids}}
+        response=requests.post("https://api.twitter.com/2/tweets/search/stream/rules",headers=header,json=to_delete)
+        if response.status_code != 200:
+            raise Exception(
+                "Cannot delete rules (HTTP {}): {}".format(
+                response.status_code, response.text
+                )
+            )
+    after_delete_rules=requests.get("https://api.twitter.com/2/tweets/search/stream/rules",headers=header)
+    #print(json.dumps(after_delete_rules.json()))
+
+    to_add={'add':req_rules}
     new_rules=requests.post("https://api.twitter.com/2/tweets/search/stream/rules",headers=header,json=to_add)
-    current_rules=requests.get("https://api.twitter.com/2/tweets/search/stream/rules",headers=header)
-    print(json.dumps(current_rules.json()))
-def main(N):
+    if new_rules.status_code != 201:
+        raise Exception(
+            "Cannot add rules (HTTP {}): {}".format(
+            new_rules.status_code, new_rules.text
+            )
+        )
+    else:    
+        current_rules=requests.get("https://api.twitter.com/2/tweets/search/stream/rules",headers=header)
+        print(json.dumps(current_rules.json())) #json.dumps----->python object to json. Serialize obj as a JSON formatted stream to fp (a .write()-supporting file-like object) using this conversion table.
+
+def twit_stream(req_rules):
+    global connection_established
+    # global stop_stream
+    print('1')
     bearer_token=os.environ.get("BEARER_TOKEN")
     bearer_str="Bearer "+str(bearer_token)
     header={"Authorization":bearer_str}
-    refine_rules(header)
-    response=requests.get('https://api.twitter.com/2/tweets/search/stream',headers=header,stream=True)
-    #print(dir(response.iter_lines))
+    refine_rules(header,req_rules)
+    
+    params={'tweet.fields':'public_metrics'}
+    #params={'tweet.fields':'created_at','tweet.fields':'referenced_tweets','tweet':'retweet_count'}
+    response=requests.get('https://api.twitter.com/2/tweets/search/stream',params=params,headers=header,stream=True)
     i=0
+    connection_established=1
     for line in response.iter_lines():
-        if i<3:
-            json_response = json.loads(line)
-            print(json.dumps(json_response, indent=4, sort_keys=True))
-            i+=1
+        global stop_stream
+        if global stop_stream==1:
+            return
         else:
-            break
+            json_response = json.loads(line)  #json.loads----->Deserialize fp (a .read()-supporting text file or binary file containing a JSON document) to a Python object using this conversion table.ie json to python object 
+            i+=1
+            temp_ls.append(json_response)
+    return
 
-if __name__=="__main__":
-    main(sys.argv[1])
 
