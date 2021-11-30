47c3c70ec6bdeefe2605042114e7b6799a98cc22
Jay Patel <69071908+Jay-Patel552@users.noreply.github.com>
2021-11-30 11:45:16 -0500

    Add files via upload

---
+++ KafkaConsumerCluster.py
@@ -0,0 +1,62 @@
+from kafka import KafkaConsumer
+import json
+import sys
+
+if __name__=='__main__':
+    N_covid=(int(sys.argv[1]))//2
+    N_vaccine = (int(sys.argv[1])) // 2
+    max_count_covid=0
+    max_count_vaccine=0
+    consumer = KafkaConsumer(bootstrap_servers=['128.110.154.221:9092','128.110.154.213:9092','128.110.154.204:9092'], auto_offset_reset='earliest',
+                             consumer_timeout_ms=1000, group_id='my_grp',value_deserializer=lambda x: json.loads(x))
+    consumer.subscribe(topics=['covid-tweets-cluster','vaccine-tweets-cluster'],pattern=None,listener=None)
+    Max_retweet_obj_covid=None
+    Max_retweet_obj_vaccine=None
+    Max_ID_covid=0
+    Max_ID_vaccine=0
+    while True:
+        msg_poll=consumer.poll(timeout_ms=1000)
+        #print("LLLLLLL")
+        for msg in msg_poll.items():
+            #print(msg)
+            for msg1 in msg[1]:
+                #print(msg1)
+                pyobj1 = msg1.value
+                if pyobj1.get('matching_rules')[0].get("tag") =="covid123":
+                    N_covid-=1
+                    if N_covid<0:
+                        pass
+                    else:
+                        if Max_ID_covid!=pyobj1.get('ID'):
+                            print('anamoly in sequence ID')
+                        Max_ID_covid=pyobj1.get('ID')
+                        if max_count_covid <= pyobj1.get('data').get('public_metrics').get('retweet_count'):
+                            max_count_covid = pyobj1.get('data').get('public_metrics').get('retweet_count')
+                            Max_retweet_obj_covid="ID: " + str(pyobj1.get('ID')) + ' Tweet with retweet count ' + str(
+                                pyobj1.get('data').get('public_metrics').get(
+                                    'retweet_count')) + ' is received from the topic:covid-tweets-cluster\n' + pyobj1.get('data').get(
+                                'text') + '\n'
+                            print(Max_retweet_obj_covid)
+                else:
+                    N_vaccine-=1
+                    if N_vaccine<0:
+                        pass
+                    else:
+                        if Max_ID_vaccine!=pyobj1.get('ID')-1:
+                            print('anamoly in sequence ID')
+                        Max_ID_vaccine=pyobj1.get('ID')
+                        if max_count_vaccine <= pyobj1.get('data').get('public_metrics').get('retweet_count'):
+                            max_count_vaccine = pyobj1.get('data').get('public_metrics').get('retweet_count')
+                            Max_retweet_obj_vaccine="ID: " + str(pyobj1.get('ID')) + ' Tweet with retweet count ' + str(
+                                pyobj1.get('data').get('public_metrics').get(
+                                    'retweet_count')) + ' is received from the topic:vaccine-tweets-cluster\n' + pyobj1.get('data').get(
+                                'text') + '\n'
+                            print(Max_retweet_obj_vaccine)
+                #print(N_covid)
+                #print(N_vaccine)
+                if N_vaccine<=0 and N_covid<=0:
+                    break
+        if N_vaccine<=0 and N_covid<=0:
+            break
+    print(Max_retweet_obj_covid)
+    print(Max_retweet_obj_vaccine)
---
+++ KafkaProducerCluster.py
@@ -0,0 +1,67 @@
+from kafka import KafkaProducer
+from stream_obtain import twit_stream
+import sys
+import json
+import os
+import time
+
+producer=KafkaProducer(bootstrap_servers=['128.110.154.221:9092','128.110.154.213:9092','128.110.154.204:9092'],linger_ms=10,value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+if __name__=='__main__':
+    id_covid=0
+    id_covid_vaccine=0
+    batch_size=int(sys.argv[1])//10+1
+    req_rules=[{"value":"covid","tag":"covid123"},{"value":'"covid vaccine"',"tag":"vaccine"}]
+    req_rules2=[{"value":'"covid vaccine"',"tag":"vaccine"}]
+    req_rules1=[{"value":"covid","tag":"covid123"}]
+    ls_covid=[]
+    ls_vaccine=[]
+    while(1):
+        temp_ls=[]
+        if id_covid <= (int(sys.argv[1])//2) and id_covid_vaccine<= (int(sys.argv[1])//2):
+            json_response_list=twit_stream(req_rules,temp_ls,batch_size)
+            if json_response_list==[]:
+                time.sleep(3)
+            else:            
+                for json_response in json_response_list:
+                    matching_rule=json_response.get("matching_rules")[0].get('tag')
+                    if matching_rule=='covid123':
+                        id_covid+=1
+                        json_response.update({'ID':id_covid})
+                        ls_covid.append(json.dumps(json_response,sort_keys=True))
+                        producer.send('covid-tweets-cluster',json_response)
+                        producer.flush()
+                    if matching_rule=='vaccine':
+                        id_covid_vaccine+=1
+                        json_response.update({'ID':id_covid_vaccine})
+                        ls_vaccine.append(json.dumps(json_response,sort_keys=True))
+                        producer.send('vaccine-tweets-cluster',json_response)
+                        producer.flush()
+        elif id_covid <= (int(sys.argv[1])//2) and id_covid_vaccine > (int(sys.argv[1])//2):
+            json_response_list=twit_stream(req_rules1,temp_ls,batch_size)
+            if json_response_list==[]:
+                time.sleep(3)
+            else:            
+                for json_response in json_response_list:
+                    id_covid+=1
+                    json_response.update({'ID':id_covid})
+                    ls_covid.append(json.dumps(json_response,sort_keys=True))
+                    producer.send('covid-tweets-cluster',json_response)
+                    producer.flush()
+        elif id_covid > (int(sys.argv[1])//2) and id_covid_vaccine <= (int(sys.argv[1])//2):
+            json_response_list=twit_stream(req_rules2,temp_ls,batch_size)
+            if json_response_list==[]:
+                time.sleep(3)
+            else:
+                for json_response in json_response_list:
+                    id_covid_vaccine+=1
+                    json_response.update({"ID":id_covid_vaccine})
+                    ls_vaccine.append(json.dumps(json_response, sort_keys=True))
+                    producer.send('vaccine-tweets-cluster',json_response)
+                    producer.flush()
+        else:
+            break
+        time.sleep(2)
+    #print(ls_covid)
+    #print("")
+    #print(ls_vaccine) 
